85c922cf6eabb2ba9d61a6398657dc45e32a6af9
szczuru <szczuru@gmail.com>
2026-08-02 16:07:01 +0200

    Refactor error codes and introduce public API
    Updated error code definitions and added public API section.

--- source/usb_mic_nx.h
+++ source/usb_mic_nx.h
@@ -10,53 +10,53 @@
  *
  * MIT License - see LICENSE in the repository root.
  */
-
+ 
 #ifndef USB_MIC_NX_H
 #define USB_MIC_NX_H
-
+ 
 #include <stdint.h>
 #include <stddef.h>
-
+ 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+ 
+/* ---- result codes -------------------------------------------------------- */
 #define MIC_NX_OK          0
-#define MIC_NX_ERR_NODEV  -1
-#define MIC_NX_ERR_INIT   -2
-#define MIC_NX_ERR_PARAM  -3
-#define MIC_NX_ERR_BUSY   -4
-
+#define MIC_NX_ERR_NODEV  -1   /* device not present / enumeration failed   */
+#define MIC_NX_ERR_INIT   -2   /* UsbHs / EP open failed                    */
+#define MIC_NX_ERR_PARAM  -3   /* bad argument                              */
+#define MIC_NX_ERR_BUSY   -4   /* already initialised                       */
+ 
+/* ---- PCM parameters the Switch UAC driver uses internally --------------- */
+/* The SingStar adapter negotiates 48 kHz mono 16-bit (UAC alt-setting 1).  */
+/* The emulated device core then resamples if the PS2 game requests 8/11 kHz */
 #define MIC_NX_SAMPLE_RATE   48000
 #define MIC_NX_CHANNELS      1
 #define MIC_NX_BITS          16
 #define MIC_NX_FRAME_BYTES   (MIC_NX_CHANNELS * (MIC_NX_BITS / 8))
+ 
+/* ---- ring-buffer capacity (frames) -------------------------------------- */
+/* 4096 frames ~ 85 ms at 48 kHz; enough headroom for 1-ms USB intervals.  */
 #define MIC_NX_RING_FRAMES   4096
-
+ 
+/* ---- known USB IDs ------------------------------------------------------- */
+/* Original SingStar dual-mic adapter (Nam Tai E&E) */
 #define SINGSTAR_VID   0x1415u
 #define SINGSTAR_PID   0x0000u
+ 
+/* Logitech USB mic */
 #define LOGITECH_VID   0x046Du
+ 
+/* Konami (AK5370) */
 #define KONAMI_VID     0x0556u
 #define KONAMI_PID     0x0001u
-
+ 
+/* ---- player index -------------------------------------------------------- */
 #define MIC_NX_PLAYER1  0
 #define MIC_NX_PLAYER2  1
 #define MIC_NX_PLAYERS  2
-
-int  mic_nx_init(void);
-void mic_nx_exit(void);
-int  mic_nx_connected(int player);
-int  mic_nx_any_connected(void);
-uint32_t mic_nx_read(int player, int16_t *dst, uint32_t frames);
-uint32_t mic_nx_frames_available(int player);
-void mic_nx_set_volume(int player, uint8_t vol);
-void mic_nx_poll(void);
-
-typedef void (*mic_nx_osd_fn)(const char *key, const char *msg, float duration);
-void mic_nx_set_osd_callback(mic_nx_osd_fn fn);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* USB_MIC_NX_H */
+ 
+/* ---- public API ---------------------------------------------------------- */
+ 
+/**
